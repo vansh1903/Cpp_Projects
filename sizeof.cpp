0336c389aef012c5b3f92014be986fcb00e40ea0
vansh1903 <80567728+vansh1903@users.noreply.github.com>
2023-05-15 09:38:09 +0530

    Delete sizeof.cpp

--- sizeof.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream.h>
-#include<conio.h>
-
-void main()
-{
-    clrscr();
-    cout<<"Size of char:1 byte\nSize of int:2 bytes\nSize of float:4 bytes\nSize of double:8 bytes";
-    getch();
-}
